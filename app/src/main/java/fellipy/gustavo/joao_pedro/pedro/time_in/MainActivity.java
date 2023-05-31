f64dcc5e4248c113bc982623cbd229f9b3f51bd2
20211tiimi0087 <jpzbarcellos@gmail.com>
2023-05-31 11:47:59 -0300

    Commit inicial

---
+++ app/src/main/java/fellipy/gustavo/joao_pedro/pedro/time_in/MainActivity.java
@@ -0,0 +1,14 @@
+package fellipy.gustavo.joao_pedro.pedro.time_in;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

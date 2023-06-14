9cc875491c0bd515526a7caedaeb1ef50a074cfc
Fellipy Wagmacker <fellipypereira@outlook.com>
2023-06-14 12:55:38 -0300

    Fellipy- Tela Cadastrar Evento e Tela editar Evento- Aula Daniel - 14-06  12:55 Terminar tela cadastrar Evento e editar Evento

--- app/src/main/java/fellipy/gustavo/joao_pedro/pedro/time_in/MainActivity.java
+++ app/src/main/java/fellipy/gustavo/joao_pedro/pedro/time_in/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent i = new Intent(MainActivity.this, Cadastro_EventoActivity.class);
+        startActivity(i);
     }
 }

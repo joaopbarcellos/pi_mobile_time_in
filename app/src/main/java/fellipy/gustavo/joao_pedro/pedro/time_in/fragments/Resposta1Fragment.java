ea1c1aceec5e8b232383a25ffaa1b7e7d3539c1b
20211tiimi0125 <pedro05042006@gmail.com>
2023-06-16 11:12:35 -0300

    Pedro - fragment resposta - 16/06 -

--- app/src/main/java/fellipy/gustavo/joao_pedro/pedro/time_in/fragments/Resposta1Fragment.java
+++ app/src/main/java/fellipy/gustavo/joao_pedro/pedro/time_in/fragments/Resposta1Fragment.java
@@ -12,10 +12,10 @@ import fellipy.gustavo.joao_pedro.pedro.time_in.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ConfiguracoesFragment#newInstance} factory method to
+ * Use the {@link Resposta1Fragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ConfiguracoesFragment extends Fragment {
+public class Resposta1Fragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class ConfiguracoesFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ConfiguracoesFragment() {
+    public Resposta1Fragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class ConfiguracoesFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ConfiguracoesFragment.
+     * @return A new instance of fragment Resposta1Fragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ConfiguracoesFragment newInstance(String param1, String param2) {
-        ConfiguracoesFragment fragment = new ConfiguracoesFragment();
+    public static Resposta1Fragment newInstance(String param1, String param2) {
+        Resposta1Fragment fragment = new Resposta1Fragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class ConfiguracoesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_configuracoes, container, false);
+        return inflater.inflate(R.layout.fragment_resposta1, container, false);
     }
 }

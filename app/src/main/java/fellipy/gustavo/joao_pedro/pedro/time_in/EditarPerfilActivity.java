0ebe8c4f149da02e3211eeb5f86df77168c71bd4
20211tiimi0087 <jpzbarcellos@gmail.com>
2023-07-05 10:56:08 -0300

    Barcellos - Coisos - Aula do Daniel 05-07

---
+++ app/src/main/java/fellipy/gustavo/joao_pedro/pedro/time_in/EditarPerfilActivity.java
@@ -0,0 +1,14 @@
+package fellipy.gustavo.joao_pedro.pedro.time_in;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditarPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_perfil);
+    }
+}

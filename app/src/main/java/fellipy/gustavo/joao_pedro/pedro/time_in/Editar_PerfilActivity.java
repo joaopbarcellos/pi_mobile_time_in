ccd85f35f335b8f6631d3a761777730fed77e08c
20211TIIMI0060 <gustavo.guguvat@gmail.com>
2023-08-08 11:05:15 -0300

    Gustavo - Aula Daniel 08/08 - Activity editar perfil arquivo

--- app/src/main/java/fellipy/gustavo/joao_pedro/pedro/time_in/Editar_PerfilActivity.java
+++ app/src/main/java/fellipy/gustavo/joao_pedro/pedro/time_in/Editar_PerfilActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class EditarPerfilActivity extends AppCompatActivity {
+public class Editar_PerfilActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

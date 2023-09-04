1501573e1494e325da6bed0e62a56fa078ca5cfa
20211tiimi0087 <jpzbarcellos@gmail.com>
2023-09-04 12:42:33 -0300

    Barcellos - Mudança de Fragment para Activity Editar_EventoActivity e EventoActivity

---
+++ app/src/main/java/fellipy/gustavo/joao_pedro/pedro/time_in/EventoActivity.java
@@ -0,0 +1,13 @@
+package fellipy.gustavo.joao_pedro.pedro.time_in;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evento);
+    }
+}

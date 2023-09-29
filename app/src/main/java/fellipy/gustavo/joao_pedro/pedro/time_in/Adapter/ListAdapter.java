8503faca5da7c43d321f3ae8f7946815a77e7eb0
Fellipy Wagmacker <fellipypereira@outlook.com>
2023-09-29 10:59:27 -0300

    Fellipy - 29-09

--- app/src/main/java/fellipy/gustavo/joao_pedro/pedro/time_in/Adapter/ListAdapter.java
+++ app/src/main/java/fellipy/gustavo/joao_pedro/pedro/time_in/Adapter/ListAdapter.java
@@ -1,11 +1,49 @@
 package fellipy.gustavo.joao_pedro.pedro.time_in.Adapter;
 
+import android.icu.text.SimpleDateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 
-import fellipy.gustavo.joao_pedro.pedro.time_in.fragments.TopEventosFragment;
+import fellipy.gustavo.joao_pedro.pedro.time_in.Evento;
+import fellipy.gustavo.joao_pedro.pedro.time_in.MyViewHolder;
+import fellipy.gustavo.joao_pedro.pedro.time_in.R;
 
-public class ListAdapter extends TopEventosFragment{
+import androidx.paging.PagingDataAdapter;
+import androidx.recyclerview.widget.DiffUtil;
 
 
-    public ListAdapter(@NonNull D)
+public class ListAdapter extends PagingDataAdapter<Evento, MyViewHolder>{
+
+
+    public ListAdapter(@NonNull DiffUtil.ItemCallback<Evento> diffCallback){
+        super(diffCallback);
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.item_recycleview_eventos, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position){
+        Evento evento = getItem(position);
+
+        // Titulo do Evento
+        TextView tvTituloItemEvento = holder.itemView.findViewById(R.id.tvTituloItemEvento);
+        tvTituloItemEvento.setText(Evento.nome);
+
+        // Data do Evento
+        TextView tvDataItemEvento = holder.itemView.findViewById(R.id.tvDataItemEvento);
+        tvDataItemEvento.setText("Data: " + new SimpleDateFormat("HH:mm dd/MM/yyyy").format(Evento.dataHorario));
+
+
+
+    }
 }
---
+++ app/src/main/java/fellipy/gustavo/joao_pedro/pedro/time_in/MyViewHolder.java
@@ -0,0 +1,14 @@
+package fellipy.gustavo.joao_pedro.pedro.time_in;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewHolder extends RecyclerView.ViewHolder{
+
+    public MyViewHolder(@NonNull View itemView){
+
+        super(itemView);
+    }
+}
